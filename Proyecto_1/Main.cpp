1dfec094dc36717475ba5dbed69a23bf11381ed1
estjhohansbello-unimilitar <est.jhohans.bello@unimilitar.edu.co>
2026-08-15 18:55:02 -0500

    Initial OpenGL project

---
+++ Proyecto_1/Main.cpp
@@ -0,0 +1,80 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main ()
+{
+	glfwInit();
+	// Your code here
+
+// Le indicamos a GLFW qué versión de OpenGL queremos utilizar.
+// En este caso: OpenGL 3.3.
+	glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 3);
+
+
+	// Le indicamos que queremos utilizar el perfil "Core" de OpenGL.
+	// Core Profile utiliza las funciones modernas de OpenGL
+	// y elimina muchas funciones antiguas.
+	glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Crea una ventana de 800x600 píxeles.
+	// "LearnOpenGL" será el título de la ventana.
+	//
+	// Los dos últimos NULL indican que no estamos usando:
+	// - un monitor específico (pantalla completa)
+	// - una ventana existente de la cual compartir recursos
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Hace que el contexto de OpenGL de nuestra ventana
+// sea el contexto actual.
+//
+// En otras palabras: le decimos a OpenGL
+// "vamos a trabajar con esta ventana".
+
+	glfwMakeContextCurrent(window);
+
+	// GLAD carga las funciones de OpenGL que necesitamos.
+	// Esto es necesario porque muchas funciones de OpenGL
+	// no están disponibles directamente en Windows.
+	gladLoadGL();
+
+	// Define el área de la ventana donde OpenGL va a dibujar.
+//
+// 0, 0 -> posición inicial (esquina inferior izquierda)
+// 800  -> ancho
+// 600  -> alto
+	glViewport(0, 0, 800, 600);
+
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+
+	// Limpia el framebuffer utilizando el color definido
+	// anteriormente con glClearColor().
+	//
+	// GL_COLOR_BUFFER_BIT indica que queremos limpiar
+	// el buffer de color.
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Intercambia los buffers.
+//
+// OpenGL normalmente utiliza un buffer que se está mostrando
+// y otro en el que se está dibujando.
+//
+// Esta función muestra en pantalla lo que acabamos de dibujar.
+	glfwSwapBuffers(window);	
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		
+	}
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+	return 0;
+}
