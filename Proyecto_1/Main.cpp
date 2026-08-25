848cb02408866e089d224532b67e84d2491bd8ed
estjhohansbello-unimilitar <est.jhohans.bello@unimilitar.edu.co>
2026-08-24 22:14:48 -0500

    Se agregaron colores a los triangulos

--- Proyecto_1/Main.cpp
+++ Proyecto_1/Main.cpp
@@ -15,7 +15,7 @@
 // se ven mas grandes en el codigo de lo que realmente ocupan en pantalla.
 
 
-/*
+/* Triangulo 
 GLfloat vertices[] =
 { //               COORDINATES                  /     COLORS           //
 	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
@@ -31,42 +31,52 @@ GLfloat vertices[] =
 
 
 GLfloat vertices[] =
-{ //          COORDINATES               /   COLORS (negro)   //
+{ //          Coordenadas  /      COLOR     /
 
+	/*
+	
+	    V0
+	   /  \
+	  /	   \
+	V1------V2
+	
+	*/	
 	// --- Oreja izquierda ---
--0.55f,  1.00f, 0.0f,   0.0f, 0.0f, 0.0f,
--0.85f,  0.50f, 0.0f,   0.0f, 0.0f, 0.0f,
-0.00f,  0.55f, 0.0f,   0.0f, 0.0f, 0.0f,
+-0.55f,  1.00f, 0.0f,   1.0f, 0.0f, 0.0f, //v0
+-0.85f,  0.50f, 0.0f,   1.0f, 0.0f, 0.0f, //v1 mmmmm
+0.00f,  0.00f, 0.0f,   1.0f, 0.0f, 0.0f, //v2  ----
 
 // --- Oreja derecha ---
-0.55f,  1.00f, 0.0f,   0.0f, 0.0f, 0.0f,
-0.00f,  0.55f, 0.0f,   0.0f, 0.0f, 0.0f,
-0.85f,  0.50f, 0.0f,   0.0f, 0.0f, 0.0f,
+0.55f,  1.00f, 0.0f,   0.0f, 0.0f, 1.0f, //v3
+0.00f,  0.00f, 0.0f,   0.0f, 0.0f, 1.0f, //v4 ----
+0.85f,  0.50f, 0.0f,   0.0f, 0.0f, 1.0f, //v5
 
  // --- Cabeza (triangulo ancho arriba, angosto abajo) ---
--0.85f,  0.50f, 0.0f,   0.0f, 0.0f, 0.0f,
-0.85f,  0.50f, 0.0f,   0.0f, 0.0f, 0.0f,
-0.00f, -0.05f, 0.0f,   0.0f, 0.0f, 0.0f,
+-0.85f,  0.50f, 0.0f,   1.0f, 0.0f, 1.0f, //v6 mmmmmm 
+0.85f,  0.50f, 0.0f,   0.0f, 0.0f, 0.0f,  //v7
+0.00f, -0.05f, 0.0f,   1.0f, 0.0f, 1.0f, //v8
 // --- Cuerpo, mitad izquierda ---
-0.00f, -0.05f, 0.0f,   0.0f, 0.0f, 0.0f,
--0.85f, -0.90f, 0.0f,   0.0f, 0.0f, 0.0f,
-0.00f, -0.90f, 0.0f,   0.0f, 0.0f, 0.0f,
+0.00f, -0.05f, 0.0f,   1.0f, 0.0f, 0.0f, //v9
+-0.85f, -0.90f, 0.0f,   0.0f, 0.0f, 0.0f, //v10
+0.00f, -0.90f, 0.0f,   0.0f, 0.0f, 1.0f, //v11
 
 // --- Cuerpo, mitad derecha ---
-0.00f, -0.05f, 0.0f,   0.0f, 0.0f, 0.0f,
-0.00f, -0.90f, 0.0f,   0.0f, 0.0f, 0.0f,
-0.85f, -0.90f, 0.0f,   0.0f, 0.0f, 0.0f,
+0.00f, -0.05f, 0.0f,   0.0f, 0.0f, 1.0f, //v12
+0.00f, -0.90f, 0.0f,   0.0f, 0.0f, 0.0f, //v13	
+0.85f, -0.90f, 0.0f,   1.0f, 0.0f, 0.0f, //v14
 
  // --- Cola, triangulo 1 ---
-0.20f, -0.90f, 0.0f,   0.0f, 0.0f, 0.0f,
-1.30f, -0.90f, 0.0f,   0.0f, 0.0f, 0.0f,
-1.60f, -0.55f, 0.0f,   0.0f, 0.0f, 0.0f,
+0.20f, -0.90f, 0.0f,   0.0f, 0.0f, 0.0f, //v15
+1.30f, -0.90f, 0.0f,   1.0f, 1.0f, 0.0f, //v16
+1.60f, -0.55f, 0.0f,   0.0f, 0.0f, 0.0f, //v17
 
- // --- Cola, triangulo 2 ---
-0.20f, -0.90f, 0.0f,   0.0f, 0.0f, 0.0f,
-1.60f, -0.55f, 0.0f,   0.0f, 0.0f, 0.0f,
-0.50f, -0.55f, 0.0f,   0.0f, 0.0f, 0.0f,
+/* // --- Cola, triangulo 2 ---
+0.20f, -0.90f, 0.0f,   0.0f, 0.0f, 0.0f, //v18
+1.60f, -0.55f, 0.0f,   0.0f, 0.0f, 0.0f, //v19
+0.50f, -0.55f, 0.0f,   0.0f, 1.0f, 1.0f,//v20
+*/
 };
+
 
 // Indices for vertices order
 /*GLuint indices[] =
