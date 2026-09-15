5410bfa4a4f282122c7fbd06b23abd14075c026a
estjhohansbello-unimilitar <est.jhohans.bello@unimilitar.edu.co>
2026-09-14 23:00:43 -0500

    Se modifico la matriz de vista

--- Proyecto_1/Main.cpp
+++ Proyecto_1/Main.cpp
@@ -213,29 +213,10 @@ int main()
         // =====================================================
         // MATRIZ DE VISTA
         // =====================================================
-
         glm::mat4 view = glm::lookAt(
-
-            // Posición de la cámara
-            glm::vec3(
-                0.0f,
-                0.0f,
-                3.0f
-            ),
-
-            // Punto al que mira
-            glm::vec3(
-                0.0f,
-                0.0f,
-                0.0f
-            ),
-
-            // Dirección hacia arriba
-            glm::vec3(
-                0.0f,
-                1.0f,
-                0.0f
-            )
+            glm::vec3(0.0f, 0.0f, 3.0f),
+            glm::vec3(0.0f, 0.0f, 0.0f),
+            glm::vec3(0.0f, 1.0f, 0.0f)
         );
 
 
